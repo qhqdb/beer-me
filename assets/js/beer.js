b26f047feef96dbe2f0425e0b4708562f0a20883
Michael Ha <mic.ha.el@me.com>
2022-07-10 14:48:27 -0700

    added script for generating random beer

---
+++ assets/js/beer.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startBtn = document.querySelector('.beerBtn')
+    const randomBeer = document.querySelector('.random-beer')
+    const descriptionDisplay = document.querySelector('.description')
+
+    function getData(e) {
+        e.preventDefault()
+
+        fetch('https://api.punkapi.com/v2/beers/random')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            const name = data[0].name
+            const abv = data[0].abv
+            const description = data[0].description
+
+            randomBeer.innerHTML = name + '  ' + abv
+            descriptionDisplay.innerHTML = description
+        })
+    }
+
+    startBtn.addEventListener('click',getData)
+
+})
