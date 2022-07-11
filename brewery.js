769934dcece43324befbe4eba09b763977218d13
Michael Ha <mic.ha.el@me.com>
2022-07-11 12:35:45 -0700

    fixed randomizer

--- brewery.js
+++ brewery.js
@@ -29,7 +29,7 @@
 // })
 
 document.addEventListener('DOMContentLoaded', () => {
-    const startBtn = document.querySelector("#randomBrew")
+    const startBtn = document.querySelector(".brewBtn")
     const randomBrew = document.querySelector(".random-brew")
     // const descriptionDisplay = document.querySelector(".descriptionbrew")
 
@@ -42,13 +42,12 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             console.log(data)
-            const name = data[0].name
-            console.log(name)
-            const brewery_type = data[0].brewery_type
+            const name = data[1].name
+            const brewery_type = data[1].brewery_type
             // console.log(brewery_type)
-            const state = data[0].state
+            const state = data[1].state
             // console.log(state)
-            const street = data[0].street
+            const street = data[1].street
             // console.log(street)
 
             randomBrew.innerHTML = name + ' ' + brewery_type + state + street
