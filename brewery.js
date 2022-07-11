198596ebf242cf9249de0551253e8394d6617d31
Wazzaubi <joeyliu7886@gmail.com>
2022-07-11 13:30:24 -0700

    no cache randomizer

--- brewery.js
+++ brewery.js
@@ -31,28 +31,44 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector("#randomBrew")
     const randomBrew = document.querySelector(".random-brew")
+    const brewType = document.querySelector(".brewType")
+    const streetAddress = document.querySelector(".streetAddress")
+    const stateBrew = document.querySelector(".state")
+    const zipcode = document.querySelector(".zipCode")
+    
     // const descriptionDisplay = document.querySelector(".descriptionbrew")
 
     function getData(e){
-        e.preventDefault()
+        // e.preventDefault()
         
-        fetch('https://api.openbrewerydb.org/breweries/random')
+        fetch('https://api.openbrewerydb.org/breweries/random',{
+            cache: "no-cache" // First time we ran this, it was saving the brewery so William came in and said we had to use this line of code so when we click "Random" it does pulled the saved aray.
+        })
         .then(response => {
+            console.log(response)
             return response.json()
         })
         .then(data => {
             console.log(data)
             const name = data[0].name
-            console.log(name)
+            // console.log(name)
             const brewery_type = data[0].brewery_type
             // console.log(brewery_type)
             const state = data[0].state
             // console.log(state)
             const street = data[0].street
             // console.log(street)
+            const postal_code = data[0].postal_code
 
-            randomBrew.innerHTML = name + ' ' + brewery_type + state + street
+            // randomBrew.innerHTML = 'Name:'+ ' ' + name + ' ' + 'Brew Type:'+ ' ' + brewery_type + ' ' + 'Street Address:'+ street + ' ' +'State:' +' '+ state
+            randomBrew.innerHTML = 'Name:'+ ' ' + name + ' '
+            brewType.innerHTML = 'Brew Type:'+ ' ' + brewery_type + ' '
+            streetAddress.innerHTML =  'Street Address:'+ ' '+ street + ' '
+            zipcode.innerHTML = 'Zip Code:'+ ' ' + postal_code
+            stateBrew.innerHTML = 'State:' +' '+ state
         })
+
+        
     }
     startBtn.addEventListener('click', getData)
 })
